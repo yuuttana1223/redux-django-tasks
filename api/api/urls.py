16bf5c7dc0835929d1c8be853eca020c1b16a835
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-03-16 15:02:11 +0900

    ✨ urlを追加

---
+++ api/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
